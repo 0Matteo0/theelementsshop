bda63fcb541f259c369e96886912bc3d0ecf67d3
JohariT5 <Joharight6@gmail.com>
2024-10-18 14:34:40 -0500

    Modifique el register y el carrito, puse todo en el form

---
+++ src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "../components/Form"
+
+function Register() {
+    return (
+        <div className="Register">
+            <Form/>
+        </div>
+    );
+}
+export default Register;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Main from '../pages/Main';
+import Register from '../pages/Register'
+import Cart from '../pages/Carrito'
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route exact path='/' element={<Main/>}/>
+          <Route exact path='/registro' element={<Register/>}/>
+          <Route exact path='/Cart' element={<Cart/>}/>
         </Routes>
       
     </Router>
