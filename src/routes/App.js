34398e043c5d1065d8887f70c1eee0126f0eaa2a
JohariT5 <Joharight6@gmail.com>
2024-10-28 16:19:40 -0500

    inicio y perfil jsx

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Main from '../pages/Main';
+import UserProfileSection from '../pages/perfil';
+import LoginForm from '../pages/inicio';
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route exact path='/' element={<Main/>}/>
+          <Route exact path='/perfil' element={<UserProfileSection/>}/>
+          <Route exact path='/inicio' element={<LoginForm/>}/>
+
         </Routes>
       
     </Router>
